be95f05e26ce1566dfd65ad7e1cfdd3dafb1e696
Johan Nijdam <johannijdam@gmail.com>
2019-05-15 15:52:04 +0200

    Add javascript to remove button

--- assets/scripts/button.js
+++ assets/scripts/button.js
@@ -2,5 +2,6 @@ $(document).ready(function () {
   $(".add-player-to-list").on('click', function () {
     var html = $(this).closest("tr").html();
     $("#my_schedules").append("<tr>" + html + "</tr>");
+    $("#my_schedules").find(".add-player-to-list").remove();
   });
 });
