cc55d40c2dcc20012bbee7c3d64e82ae1b121155
Johan Nijdam <johannijdam@gmail.com>
2019-05-16 19:52:34 +0200

    Added sql ifs

--- helper.class.cb.php
+++ helper.class.cb.php
@@ -9,9 +9,10 @@ class HelperClass {
 		// $this->connection = new mysqli("localhost", "root", "root", "fifa");
 	}
 
-	function getPlayerPositions($set = null) {
+	function getPlayerPositions($set) {
 		$set = preg_replace("/[^a-zA-Z]/", "", $set);
-		if($set != null){
+
+		if($set != "sub"){
 			$q = "SELECT * FROM players WHERE Position LIKE '".$set."' ORDER BY RAND() LIMIT 5";
 		} else {
 			$q = "SELECT * FROM players ORDER BY RAND() LIMIT 5";
@@ -32,13 +33,13 @@ class HelperClass {
 
 	
 
-	function getShuffledPlayerPositions($set) {
+	function getShuffledPlayerPositions($set = null) {
 		$positions = $this->getPlayerPositions($set);
 		return $positions;
 	}
 
 	function getPositions() {
-		return ['CB1','CB2','CB3', 'GK1', 'CM1', 'CM2', 'CM3', 'CM4', 'LM1' ,'CAM1' ,'RM1', 'ST1','ST2', 'LW1', 'RW1', 'LB1', 'RB1', 'CDM1'];
+		return ['CB1','CB2','CB3', 'GK1', 'CM1', 'CM2', 'CM3', 'CM4', 'LM1' ,'CAM1' ,'RM1', 'ST1','ST2', 'LW1', 'RW1', 'LB1', 'RB1', 'CDM1', '#Random1','SUB2','SUB3', 'SUB4', 'SUB5', 'SUB6', 'SUB7', 'SUB8'];
 	}
 }
 ?>
